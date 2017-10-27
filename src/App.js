db95b0da2ba6345a81bd07d68e09257ce5d6587c
wangwan <wangwan0428@163.com>
2017-10-27 11:21:23 +0800

    add some contacts

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
+class ContactsList extends React.Component {
+  render() {
+    const people = [
+      {name: 'jack'},
+      {name: 'rose'},
+      {name: 'andMe'}
+    ];
+
+    return <ol>
+      {people.map(people => (
+        <li key = {people.name}>{people.name}</li>
+      ))}
+    </ol>
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ContactsList/>
       </div>
     );
   }
