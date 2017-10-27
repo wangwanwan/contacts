259b622220834e1b9f0d391903c19ad95e6da395
wangwan <wangwan0428@163.com>
2017-10-27 14:22:14 +0800

    remove some useless files and contents

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
 import React, { Component } from 'react';
 
 
-class ContactsList extends React.Component {
-  render() {
-    const people = this.props.contacts;
-
-    return <ol>
-      {people.map(people => (
-        <li key = {people.name}>{people.name}</li>
-      ))}
-    </ol>
-  }
-}
-
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <ContactsList contacts={[
-          {name: 'jack'},
-          {name: 'rose'},
-          {name: 'andMe'}        
-        ]}/>
-        <ContactsList contacts={[
-          {name: 'xuchen'},
-          {name: 'mingliang'},
-          {name: 'tanshi'}
-        ]}/>
-      </div>
-    );
+
   }
 }
 
