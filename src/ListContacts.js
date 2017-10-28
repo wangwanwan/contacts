ba39e48605bccb19ba6fb9d883fa6977db0e2c5f
wangwan <wangwan0428@163.com>
2017-10-28 10:36:06 +0800

    add contact avatar,name,email and remove button

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -6,8 +6,16 @@ class ListContacts extends Component {
     return (
       <ol className="contact-list">
         {this.props.contacts.map(contact => (
-          <li key={contact.id}>
-            {contact.name}
+          <li key={contact.id} className="contact-list-item">
+            <div className="contact-avatar" style={{
+              backgroundImage: `url(${contact.avatarURL})`
+            }}>
+            </div>
+            <div className="contact-details">
+              <p>{contact.name}</p>
+              <p>{contact.email}</p>
+            </div>
+            <button className="contact-remove"></button>
           </li>
         ))}
       </ol>
