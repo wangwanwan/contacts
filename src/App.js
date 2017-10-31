f1b848800a6f99c6d612f90599507f7b323618da
wangwan <wangwan0428@163.com>
2017-10-31 20:43:08 +0800

    Use Route to control the back key

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import ListContacts from './ListContacts';
 import CreateContact from './CreateContact';
 import * as ContactsAPI from './utils/ContactsAPI';
@@ -6,7 +7,6 @@ import * as ContactsAPI from './utils/ContactsAPI';
 class App extends Component {
 
   state = {
-    screen: 'list', // list or create
     contacts: []
   }
 
@@ -27,21 +27,17 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        {(this.state.screen === 'list') && (
+        <Route exact path='/' render={() => (
           <ListContacts
             onRemoveContact={this.removeContact}
             contacts={this.state.contacts}
-            onNavigate={() => {
-              this.setState({ screen: 'create' })
-            }}
           />
-        )}
-        {(this.state.screen === 'create') && (
-          <CreateContact/>
-        )}
-      </div>            
+        )}/>
+        <Route path='/create' component={CreateContact}/>
+      </div>
     )
   }
+
 }
 
 export default App;
